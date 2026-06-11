f5a843c33d55ee2ed002349ae3be833821cc8fd8
Hyndrrx <hyndrrx0@gmail.com>
2026-06-12 01:58:46 +0200

    c

--- invoicepro/settings.py
+++ invoicepro/settings.py
@@ -79,13 +79,20 @@ TEMPLATES = [
 #}
 
 #===================POSTGRESSQL=================
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ["DATABASE_URL"],
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+    
 # ================= PASSWORD VALIDATION =================
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
